37ee2d75089694c644d77789cd5f876e046d7b9e
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-05 17:52:57 +0300

    Extract sensitive Auth0 credentials into env config

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  // @ts-nocheck
   import { Auth0Context } from '@dopry/svelte-auth0'
   import { Router } from '@roxi/routify'
   import { routes } from '../.routify/routes'
@@ -6,9 +7,9 @@
 </script>
 
 <Auth0Context
-  domain="eseuri-prod.eu.auth0.com"
-  client_id="NJgHdaXoixiNUpEks7b3TtYi95ZH433P"
-  audience="https://eseuri-prod.eu.auth0.com/api/v2/"
+  domain={import.meta.env.VITE_AUTH0_DOMAIN}
+  client_id={import.meta.env.VITE_AUTH0_CLIENT_ID}
+  audience={import.meta.env.VITE_AUTH0_AUDIENCE}
   callback_url={window.location.origin}
   logout_url={window.location.origin}
 >
